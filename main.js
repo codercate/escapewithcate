2218df020f472aa8594058b483fff50f8c936c14
Cate <cate@Cates-MacBook.local>
2021-06-26 11:39:41 -0700

    added img files and completed navBar

---
+++ main.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var x = document.getElementById("slideout-menu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
